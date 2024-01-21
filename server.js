3301948d1443c4b323d5b8cea1e269699b8a95d0
ayamoosa <ayamoosa98@gmail.com>
2024-01-20 22:51:34 -0800

    TO-DO: set up db'

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.use(express.urlencoded({extended:true}))
 
 app.use(express.json())
 
+//set up database and create middlewear
+
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
